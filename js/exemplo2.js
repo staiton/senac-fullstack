0f21ebb75d631a4064faaeecd9bf625c9f8a2f1c
Gabriel França <gabrielfranca5592@gmail.com>
2023-06-19 21:26:17 -0300

    Espaços alterados

--- js/exemplo2.js
+++ js/exemplo2.js
@@ -4,13 +4,13 @@ let entradaInvalida = isNaN(nota) || nota < 0 || nota > 10;
 let aprovado = nota >= 5;
 let reprovado = nota < 3;
 
-if (entradaInvalida) {
+if (entradaInvalida){
     alert('Entrada inválida \n Digite novamente');
-} else if (aprovado) {
+} else if (aprovado){
     alert('Parabens! Você está aprovado')
-} else if (reprovado) {
+} else if (reprovado){
     alert('Infelizmente você está reprovado.')
-}  else {
+}  else{
     alert('Quase lá, você está em recuperação.')
 }
 
