3d8d873e79db5c90cb40b7fcd7ca566c3c3e93e0
Gabriel França <gabrielfranca5592@gmail.com>
2023-06-26 21:27:56 -0300

    Teste

---
+++ js/lista-wiki/exemplo-switch.js
@@ -0,0 +1,21 @@
+let letra = `a`;
+
+switch(letra){
+    case `a`:
+        alert(`letra A`);
+        break;
+    case `e`:
+        alert(`letra E`);
+        break;
+    case `i`:
+        alert(`letra I`);
+        break;
+    case `o`:
+        alert(`letra O`);
+        break;
+    case `u`:
+        alert(`letra U`);
+        break;
+    default:
+        alert(`Não é vogal`);
+}
