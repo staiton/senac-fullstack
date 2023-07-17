1d0a308311b3e144faa0ef4a381dea973a7c3032
Gabriel França <gabrielfranca5592@gmail.com>
2023-07-17 20:03:39 -0300

    Apenas enviando

--- js/orientacao-objeto/carro.js
+++ js/orientacao-objeto/carro.js
@@ -1,3 +1,13 @@
-class Carro {
-    
+export class Carro {
+    // atributos
+    marca;
+    modelo;
+    ano;
+    // constructor
+    constructor(modelo, marca, ano){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+    // métodos
 }
---
+++ js/orientacao-objeto/main.js
@@ -0,0 +1,7 @@
+import {Carro} from './carro.js';
+
+// Instanciando o objeto up
+const up = new Carro(`Up`, `VM`, 2015);
+
+// Instanciando o objeto ka
+const ka = new Carro(`Ka`, `Ford`, 2006);
